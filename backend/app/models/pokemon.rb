f74e784fca4a308a6b851865a62495f8c65061e3
Xi Duo Zhang <dzxd.ee@gmail.com>
2025-10-17 11:41:30 -0400

    pokemon controller and model

--- backend/app/models/pokemon.rb
+++ backend/app/models/pokemon.rb
@@ -1,5 +1,20 @@
 class Pokemon < ApplicationRecord
   has_one_attached :image
 
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :poke_index, :hp, :attack, :defense, :speed_attack, :speed_defense, :speed, presence: true
 
+  after_save :accumulate_all_into_total
+
+  def calculate_total
+    self.hp + self.attack + self.defense + self.speed_attack + self.speed_defense + self.speed
+  end
+
+  def accumulate_all_into_total
+    self.update_column(:total, self.calculate_total)
+  end
+
+  def is_legendary?
+    self.legendary
+  end
 end
